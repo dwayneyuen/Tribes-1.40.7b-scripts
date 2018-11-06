bffb1c247828287fc7a29c777d218e6770860b22
dwayneyuen <dwayneyuen@gmail.com>
2018-11-05 21:30:43 -0800

    Comment out missing common files
    These files are already loaded from the search path, I don't know what these are for. They just throw errors.

--- common.cs
+++ common.cs
@@ -1,23 +1,23 @@
-exec( "Run" );
+// exec( "Run" );
 
-rundir("");
-run( "ActionMaps" );
-run( "Util" );
-run( "Autoload" );
-run( "Event" );
-run( "Containers" );
-run( "HuffFreqString" );
-run( "Include" );
-run( "Schedule" );
-run( "Sprintf" );
-run( "Stack" );
-run( "Timestamp" );
+// rundir("");
+// run( "ActionMaps" );
+// run( "Util" );
+// run( "Autoload" );
+// run( "Event" );
+// run( "Containers" );
+// run( "HuffFreqString" );
+// run( "Include" );
+// run( "Schedule" );
+// run( "Sprintf" );
+// run( "Stack" );
+// run( "Timestamp" );
 
-rundir( "stats/" );
-run( "loadall" );
+// rundir( "stats/" );
+// run( "loadall" );
 
-rundir( "tags/" );
-run( "loadall" );
+// rundir( "tags/" );
+// run( "loadall" );
 
 rundir( "UnitTests/" );
 run( "loadall" );
